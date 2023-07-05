39c697b4ff561ad4d6b3677f91de3cd64bdb9086
Vladimir_Bavtenko <batonich84@mail.ru>
2023-07-05 13:38:29 +0300

    Watch component implemented.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import Header from '../components/Header/Header';
 import Main from '../components/Main/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Main/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
 import styles from './watch.module.scss'
 
-class Watch extends Component {
+interface IWatchProps {
+
+}
+
+interface IWatchState {
+  date: Date;
+}
+
+type Props = Readonly<IWatchProps>
+type State = Readonly<IWatchState>
+
+class Watch extends Component<Props, State> {
+  private timerId: NodeJS.Timer | undefined;
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.setState({date: new Date()}), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
   render() {
+    const {date} = this.state
+
     return (
-      <div>
-        Watch
+      <div className={styles.watch}>
+        {date.toLocaleTimeString()}
       </div>
     );
   }
